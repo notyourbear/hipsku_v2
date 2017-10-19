6f7e8f86e89ead8ad74fcf69dff289ceb88eaab2
notyourbear <pracenberg@gmail.com>
2017-10-18 21:27:04 -0700

    css starts

--- src/server/render-app.js
+++ src/server/render-app.js
@@ -9,11 +9,16 @@ const renderApp = (array) => {
     <!doctype html>
     <html>
       <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="https://fonts.googleapis.com/css?family=Fredericka+the+Great|Love+Ya+Like+A+Sister" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css">
         <link rel="stylesheet" href="${config.STATIC_PATH}/style.css">
         <title>Hipsku</title>
       </head>
       <body>
+        <div class="overlay"></div>
         <div class="container">
           ${haiku}
         </div>
